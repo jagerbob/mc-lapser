c4b5afde50ed02e80d470bb9d8a91b764710dabf
jagerbob <91737697+jagerbob@users.noreply.github.com>
2023-07-19 19:33:26 +0200

    Initial commit

---
+++ src/main/java/com/jagerbob/lapser/LapserMod.java
@@ -0,0 +1,15 @@
+package com.jagerbob.lapser;
+
+import com.jagerbob.lapser.constants.Config;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LapserMod implements ModInitializer {
+    private final Logger logger = LoggerFactory.getLogger(Config.MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/jagerbob/lapser/constants/Config.java
@@ -0,0 +1,7 @@
+package com.jagerbob.lapser.constants;
+
+public class Config {
+
+    public static final String MOD_ID = "lapser";
+
+}
